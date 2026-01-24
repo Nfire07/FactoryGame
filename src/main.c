9d9ea75b88b2fc3fb3b64465813c55cb675e50e2
Nfire07 <nfire2007@gmail.com>
2026-01-24 23:18:25 +0100

    project structure defined

--- src/main.c
+++ src/main.c
@@ -1,17 +1,92 @@
 #include <raylib.h>
+#include <stdio.h>
 
-int main(void){
-	
-	InitWindow(500,500,"Factory Game");
+int screenWidth = 0;
+int screenHeight = 0;
 
-	while(!WindowShouldClose()){
-		if(IsKeyPressed(22)) break;
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		EndDrawing();
-	}	
+/*
+ * Graphics
+ *  - a grid zoomable +-
+ *  - a top bar with global resources and a way to navigate
+ *  - a way to create insert destroy and manipulate blocks and place rails
+ *
+ * We need 4 biomes
+ * 
+ * plains
+ * mountains
+ * sea
+ * hills
+ *
+ */
+
+void Setup(const char* name){
+	int monitor = GetCurrentMonitor();
+
+	screenWidth = 2560;
+        screenHeight = 1440;
+
+	InitWindow(screenWidth,screenHeight,name);
+
+	ToggleFullscreen();
+
+	SetTargetFPS(60);
+}
+
+void Update(float delta){
+
+}
+
+void DrawGrid(int tileSize, int dx, int dy, Color gridColor){
+	for(int i=1;i<screenHeight/tileSize + 1;i++){
+		DrawLine(0,i*tileSize,screenWidth,i*tileSize,gridColor);
+	}
+
+	for(int i=1;i<screenWidth/tileSize + 1;i++){
+		DrawLine(i*tileSize,0,i*tileSize,screenHeight,gridColor);
+	}
+}
+
+void Render(){
+	BeginDrawing();
+
+	ClearBackground(GetColor(0x212121ff));
+	DrawGrid(50,0,0,GRAY);
+
+	EndDrawing();	
+}
+
+void Resize(int width,int height){
+
+}
+
+void Input(){
+
+}
+
+void Close(){
 
 	CloseWindow();
+
+}
+
+int main(void){
+
+	Setup("Factory");
+
+	while(!WindowShouldClose()){
+		float delta = GetFrameTime();
+
+		Update(delta);
+
+		Render();
+
+		Input();
+
+		if(IsWindowResized())
+			Resize(0,0);
+	}	
+
+	Close();
 
 	return 0;
 }
